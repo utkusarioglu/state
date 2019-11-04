de4a476833506f99b2bd4f8b0b2abc76ca8d639e
Utku Sarioglu <utkusarioglu@hotmail.com>
2019-11-04 18:31:49 +0300

    Non-library version

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,6 @@ module.exports = {
         path: __dirname,
         filename: '[name].js',
         libraryTarget: 'amd',
-        library: 'Lib',
         umdNamedDefine: true
     },
     resolve: {
