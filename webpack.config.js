1e0ae6523ea6aea9364ffcaa2a1f90c4d10ea72b
Utku Sarioglu <utkusarioglu@hotmail.com>
2019-11-04 18:36:33 +0300

    Switch to commonjs

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     output: {
         path: __dirname,
         filename: '[name].js',
-        libraryTarget: 'amd',
+        libraryTarget: 'commonjs',
         umdNamedDefine: true
     },
     resolve: {
